413408d3db9c18f97791ff5116dc55e4ec10e68f
n-d-u <nandakishores73@gmail.com>
2021-12-04 12:26:06 +0530

    First commit

---
+++ ToJSON.py
@@ -0,0 +1,39 @@
+import json
+
+# states = input("Enter States\n").split()
+# letters = input("Enter letters\n").split()
+# tf = input("TF\n").split()
+# tf2 = [[]*3]*len(tf)
+#
+# for i in range(len(tf2)):
+#     tf2[i] = tf[i].split(":")
+#
+# startStates = input("Enter Start States\n").split()
+# finalStates = input("Enter Final States\n").split()
+# x = {
+#     "states": states,
+#     "letters": letters,
+#     "transition_function": tf2,
+#     "transition_matrix": tf2,
+#     "start_states": startStates,
+#     "final_states": finalStates
+# }
+#
+# y = json.dumps(x)
+# with open("sample.json", "w") as outfile:
+#     outfile.write(y)
+
+
+def w2json(states, letters, tf2, startStates, finalStates):
+    x = {
+        "states": states,
+        "letters": letters,
+        "transition_function": tf2,
+        # "transition_matrix": tf2,
+        "start_states": startStates,
+        "final_states": finalStates
+    }
+    y = json.dumps(x)
+    with open("inputMain.json", "w") as outfile:
+        outfile.write(y)
+
